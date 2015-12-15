0ea69c46378b267d4286c4fe07914a2a2d9463c1
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-12-14 20:04:04 -0700

    FULLSCREEN Yeah!

--- examples/src/components/molecules/Canvas.js
+++ examples/src/components/molecules/Canvas.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var _Canvas = require('src/Canvas');
 var ResizeSensor = require('react-resize-sensor');
+var requestFullscreen = require('../../modules/requestFullscreen');
 
 var Canvas = React.createClass({
   propTypes: {
@@ -18,7 +19,7 @@ var Canvas = React.createClass({
     var style = this._style();
 
     return (
-      <div style={style.root}>
+      <div ref="root" style={style.root} onClick={this._fullscreen}>
         <canvas ref="canvas" style={style.canvas}></canvas>
         <ResizeSensor onResize={this._queueDraw} />
       </div>
@@ -31,6 +32,10 @@ var Canvas = React.createClass({
 
   componentDidUpdate: function () {
     this._queueDraw();
+  },
+
+  _fullscreen: function () {
+    requestFullscreen(this.refs.root);
   },
 
   _style: function () {
---
+++ examples/src/modules/requestFullscreen.js
@@ -0,0 +1,11 @@
+module.exports = function requestFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+}
